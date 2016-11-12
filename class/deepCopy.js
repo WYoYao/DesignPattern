e4f7431362d6499cb2178dc9a1bdb818ccc4b4a8
leoWang <492260726@qq.com>
2016-11-12 15:52:20 +0800

    实现深度Copy 合并

--- class/deepCopy.js
+++ class/deepCopy.js
@@ -1,32 +1,126 @@
 
 /**
- * 实现深Copy
+ * 通过深度Copy将多个Object类型合并到一个Object 中从前向后执行 
  */
 
-function deepCopy(target, source) {
-    if (Object.prototype.toString.call(target) != '[object Object]' || Object.prototype.toString.call(source) != '[object Object]') throw new Error('Arguments Error');
-
-    var key;
-
-    for (key in source) {
-        if (source.hasOwnProperty(key)) {
-            if (Object.prototype.toString.call(source[key]) == '[object Object]') {
-                target[key] = {};
-                deepCopy(target[key], source[key]);
-            } else if (Object.prototype.toString.call(source[key]) == '[object Array]') {
-                // target[key]=[];
-                target[key] = source[key].map(function (item, index) {
-                    var res;
-
-                })
+function Copy() {
+    // 判断数据类型
+    function _valiteType(value) {
+        return {
+            '[object Object]': 'Object',
+            '[object Array]': 'Array',
+            '[object Base]': 'Base',
+        }[Object.prototype.toString.call(value)]
+    }
+    // 执行复制动作
+    function _copy(content,item) {
+        var type=_valiteType(item);
+        if (type == 'Object' || type == 'Array') {
+            if(_valiteType(content)!=type){
+                // 如果类型不是对应的需要以后面需要copy的类型为准
+                if(type=='Array'){
+                    content=[];
+                }else{
+                    content={};
+                }
             }
+            return _exec(content,item,type);
+        } else {
+            return item;
         }
     }
 
+    // 复制返回的对应的引用类型
+    function _exec(content,obj){
+        var key;
+        for(key in obj){
+            if(obj.hasOwnProperty(key)){
+                content[key]=_copy(content[key],obj[key]);
+            }
+        }
+        return content;
+    }
+
+    this.copy=function(){
+        var argu=Array.prototype.slice.call(arguments);
+
+        argu.forEach(function(item){
+            if (Object.prototype.toString.call(item) != '[object Object]') throw new Error('Arguments Error');
+        })
+        // 循环执行所有的参数
+        return argu.reduce(function(content,item){
+            return _copy(content,item);
+        },{})
+    }
 }
 
-console.log(Object.prototype.toString.call([]))
 
-function test(arr) {
-    arr.push(1);
+
+
+var demo=new Copy;
+
+var source = {
+    name: 'leo',
+    age: 24,
+    agelist: [1,2,3,4,5],
+    like: {
+        play: 'JS',
+        parter: ['parter'],
+        color: {
+            red: '99',
+        }
+    },
+    display: ['block', ['parter'], {
+        red: '99',
+    }]
 }
+
+var source1 = {
+    name: 'leo',
+    age: 25,
+    like: {
+        play: 'JS',
+        parter: ['parter1'],
+        color: {
+            red: '99',
+        }
+    },
+    display: ['block', ['parter1'], {
+        red: '99',
+    }]
+}
+
+var source2 = {
+    name: 'leo',
+    age: 25,
+    like: {
+        play: 'JS',
+        parter: ['parter1'],
+        color: {
+            red: '99',
+        }
+    },
+    display: {
+        0:'blockObj',
+        1:['parterObj'],
+        2:{
+            red: '99Obj',
+        }
+    }
+}
+
+
+var target = demo.copy(source,source1,source2);
+console.log(target);
+console.log(target.like == source.like);
+console.log(target.display == source.display);
+console.log(target.like.parter == source.like.parter);
+console.log(target.like.color == source.like.color);
+console.log(target.display[1] == source.display[1]);
+console.log(target.display[2] == source.display[2]);
+
+
+
+
+
+
